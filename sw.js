d3d29aeb20f7ed6778e51137d35759d951fc0147
guidopelusobarreto-spec <guidopelusobarreto@gmail.com>
2026-09-20 10:17:53 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'guidopro-v46';
+const CACHE_NAME = 'guidopro-v47';
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['/'])));
   self.skipWaiting();
